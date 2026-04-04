87c8b805f07eb00eff423df9bcab8d7c385db14e
joão pedro <joaopedro251900@gmail.com>
2026-04-04 16:12:52 -0300

    flow product

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:21806',
+        target: 'https://backbarbearia-69p3.onrender.com',
         changeOrigin: true,
       },
     },
